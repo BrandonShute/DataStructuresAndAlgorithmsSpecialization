e0e1e852e7b7ac5828708e7942f29232ad49961e
BrandonShute <brandon.shute@kooltra.com>
2019-11-17 18:37:32 -0500

    Implement Primative calculator week 5 part 1/3

--- AlgorithmicToolbox/src/main/java/personal/brandonshute/coursera/week5/PrimitiveCalculator.java
+++ AlgorithmicToolbox/src/main/java/personal/brandonshute/coursera/week5/PrimitiveCalculator.java
@@ -1,32 +1,92 @@
 package personal.brandonshute.coursera.week5;
 
 import java.util.*;
+import java.util.function.Function;
 
+/**
+ * Primitive calculator under problem under the week 5 assignment.
+ */
 public class PrimitiveCalculator {
-    private static List<Integer> optimal_sequence(int n) {
-        List<Integer> sequence = new ArrayList<Integer>();
-        while (n >= 1) {
-            sequence.add(n);
-            if (n % 3 == 0) {
-                n /= 3;
-            } else if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n -= 1;
-            }
-        }
-        Collections.reverse(sequence);
-        return sequence;
+
+	private static final Map<Calculation, Function<Integer, Integer>> INVERSE_CALCULATION_FACTORY = Map.of(
+		Calculation.ADD_1, (value) -> value - 1,
+		Calculation.MULTIPLY_2, (value) -> value / 2,
+		Calculation.MULTIPLY_3, (value) -> value / 3
+	);
+
+
+    public static List<Integer> getOptimalSequence(final int value) {
+    	if (value == 1) {
+    		return List.of(1);
+		}
+
+    	// Setup initial values
+    	Map<Integer, Integer> valueToNumOperations = new HashMap<>();
+    	valueToNumOperations.put(1, 0);
+    	valueToNumOperations.put(2, 1);
+
+    	Map<Integer, Integer> valueToPreviousValue = new HashMap<>();
+		valueToPreviousValue.put(1, -1);
+		valueToPreviousValue.put(2, 1);
+
+    	for (Integer i = 3; i <= value; i++) {
+    		final Integer currentValue = i;
+    		final Calculation calcToApply = getValidCalculations(i).stream()
+					.map((calc) -> {
+						final Integer previousValue = INVERSE_CALCULATION_FACTORY.get(calc).apply(currentValue);
+						return Map.entry(calc, valueToNumOperations.get(previousValue));
+					})
+					.min(Comparator.comparing(Map.Entry::getValue))
+					.get()
+					.getKey();
+
+			final Integer previousValue = INVERSE_CALCULATION_FACTORY.get(calcToApply).apply(currentValue);
+			final Integer previousNumOperations = valueToNumOperations.get(previousValue);
+
+    		valueToNumOperations.put(i, previousNumOperations + 1);
+    		valueToPreviousValue.put(i, previousValue);
+		}
+
+		return getFullSequenceFromMap(valueToPreviousValue);
     }
+
+    private static List<Calculation> getValidCalculations(final Integer value) {
+    	List<Calculation> validCalculations = new ArrayList<>();
+    	validCalculations.add(Calculation.ADD_1);
+    	if (value % 2 == 0) {
+    		validCalculations.add(Calculation.MULTIPLY_2);
+		}
+		if (value % 3 == 0) {
+			validCalculations.add(Calculation.MULTIPLY_3);
+		}
+		return validCalculations;
+	}
+
+	private static List<Integer> getFullSequenceFromMap(final Map<Integer, Integer> valueToPreviousValue) {
+		Integer currentValue = valueToPreviousValue.size();
+		List<Integer> optimalSequence = new ArrayList<>();
+		while (currentValue >= 1) {
+			optimalSequence.add(currentValue);
+			currentValue = valueToPreviousValue.get(currentValue);
+		}
+		Collections.reverse(optimalSequence);
+		return optimalSequence;
+	}
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        List<Integer> sequence = optimal_sequence(n);
+        List<Integer> sequence = getOptimalSequence(n);
         System.out.println(sequence.size() - 1);
         for (Integer x : sequence) {
             System.out.print(x + " ");
         }
     }
+
+    private enum Calculation {
+		MULTIPLY_3,
+		MULTIPLY_2,
+		ADD_1
+	}
 }
 
---
+++ AlgorithmicToolbox/src/test/java/personal/brandonshute/coursera/week5/PrimitiveCalculatorTest.java
@@ -0,0 +1,82 @@
+package personal.brandonshute.coursera.week5;
+
+import org.junit.Test;
+import personal.brandonshute.coursera.Fixture;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class PrimitiveCalculatorTest {
+
+	// Problem constrains
+	final static int MAX_VALUE = 100_000;
+
+	@Test
+	public void when_provided_with_one_then_return_sequence_containing_one() {
+		final List<Integer> result = PrimitiveCalculator.getOptimalSequence(1);
+
+		assertThat(result.size(), is(1));
+		assertThat(result.get(0), is(1));
+	}
+
+	@Test
+	public void when_provided_with_simple_input_then_return_correct_sequence() {
+		List<Integer> expectedList = new ArrayList<>();
+		expectedList.add(1);
+		expectedList.add(3);
+		expectedList.add(4);
+		expectedList.add(5);
+
+		final List<Integer> result = PrimitiveCalculator.getOptimalSequence(5);
+
+		assertThat(result, is(expectedList));
+	}
+
+	@Test
+	public void when_provided_unsafe_move_of_ten_then_should_complete_in_three_moves() {
+		List<Integer> expectedList = new ArrayList<>();
+		expectedList.add(1);
+		expectedList.add(3);
+		expectedList.add(9);
+		expectedList.add(10);
+
+		final List<Integer> result = PrimitiveCalculator.getOptimalSequence(10);
+
+		assertThat(result, is(expectedList));
+	}
+
+	@Test
+	public void when_provided_large_value_where_greedy_algorithm_will_not_work_then_return_correct_output() {
+		List<Integer> expectedList = new ArrayList<>();
+		expectedList.add(1);
+		expectedList.add(3);
+		expectedList.add(9);
+		expectedList.add(10);
+		expectedList.add(11);
+		expectedList.add(33);
+		expectedList.add(99);
+		expectedList.add(297);
+		expectedList.add(891);
+		expectedList.add(2673);
+		expectedList.add(8019);
+		expectedList.add(16038);
+		expectedList.add(16039);
+		expectedList.add(48117);
+		expectedList.add(96234);
+
+		final List<Integer> result = PrimitiveCalculator.getOptimalSequence(96234);
+
+		assertThat(result, is(expectedList));
+	}
+
+	@Test
+	public void when_provided_large_list_without_element_then_return_negative_one_in_allowable_time() {
+		long startTime = System.currentTimeMillis();
+		assertThat(PrimitiveCalculator.getOptimalSequence(MAX_VALUE).size(), greaterThan(0));
+		assertThat(System.currentTimeMillis() - startTime < Fixture.MAX_ALLOWABLE_CALCULATION_TIME, is(true));
+	}
+}
